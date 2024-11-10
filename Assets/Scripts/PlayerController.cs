c8d3c119000eb2d65debb5c8b99d6d851a9f417d
Justin-Lam <justinflam9225k@gmail.com>
2024-11-09 18:09:00 -0800

    Refined horizontal movement max velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float acceleration;
-    [SerializeField] float maxVelocity;
+	[Header("Horizontal Movement")]
+	[SerializeField] float acceleration;
+	[SerializeField] float maxVelocity;
 
-    Rigidbody rb;
-    Vector3 moveDirection;
+	[Header("Jumping")]
+	[SerializeField] float jumpStrength;
+
+	Rigidbody rb;
+	Vector3 moveDirection;
 
 
-    void Start()
-    {
-        // Get rb
-        rb = GetComponent<Rigidbody>();
+	void Start()
+	{
+		// Get rb
+		rb = GetComponent<Rigidbody>();
+	}
 
-        // Set max velocity
-        rb.maxLinearVelocity = maxVelocity;
-    }
+	void Update()
+	{
+		// Get move direction
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
-    void Update()
-    {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-    }
+		// Check for jump
+		if (Input.GetButtonDown("Jump"))
+		{
+			// Jump
+			rb.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+		}
+	}
 
 	void FixedUpdate()
 	{
-        rb.AddRelativeForce(moveDirection * acceleration * Time.deltaTime);
+		// Apply movement force
+		rb.AddRelativeForce(moveDirection * acceleration * Time.deltaTime, ForceMode.Acceleration);
+
+		// Limit movement velocity
+		Vector3 horizontalVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+		if (horizontalVelocity.magnitude > maxVelocity)
+		{
+			// Calculate the counter-force needed to maintain max velocity
+			float amountOverMaxVelocity = horizontalVelocity.magnitude - maxVelocity;
+			Vector3 counterForce = -horizontalVelocity.normalized * amountOverMaxVelocity;
+
+			// Apply counter-force
+			rb.AddRelativeForce(counterForce, ForceMode.VelocityChange);
+		}
 	}
 }
