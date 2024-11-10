17fb043e6978b598456827c0f964aab69e3553ce
Justin-Lam <justinflam9225k@gmail.com>
2024-11-09 19:29:07 -0800

    Implemented jump buffering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,17 @@ public class PlayerController : MonoBehaviour
 
 	[Header("Jumping")]
 	[SerializeField] float jumpStrength;
+	[SerializeField] float jumpBufferDuration;
+	[SerializeField] float letGoOfJumpVelocityDecreaseRatio;
 	[SerializeField] float extraGravityWhenFallingAcceleration;
 
 	Rigidbody rb;
 	Vector3 moveDirection;
-	float groundedDistFromGround;	// the max distance the player can be from the ground in order to be grounded
+	float jumpBufferCounter = 0;
+	bool fastFalling = true;
+	bool usedFastFall = false;
+	float groundedDistFromGround;   // the max distance the player can be from the ground in order to be grounded
+	float groundedDistFromGroundPadding = 0.1f;	// (10%)
 
 
 	void Start()
@@ -22,7 +28,7 @@ public class PlayerController : MonoBehaviour
 
 		// Calculate groundedDistFromGround
 		float colliderRadius = GetComponent<SphereCollider>().radius;
-		groundedDistFromGround = colliderRadius * 1.1f;	// add some padding (10%)
+		groundedDistFromGround = colliderRadius * (1 + groundedDistFromGroundPadding);
 	}
 
 	void Update()
@@ -30,14 +36,28 @@ public class PlayerController : MonoBehaviour
 		// Get move direction
 		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
-		// Check for jump
-		if (Input.GetButtonDown("Jump") && IsGrounded())
+		// Handle jump buffer
+		if (Input.GetButtonDown("Jump"))
 		{
-			// Set y velocity to 0 so in case we're falling our jump isn't weakened by the current downwards velocity
-			rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+			jumpBufferCounter = jumpBufferDuration;
+		}
+		if (jumpBufferCounter > 0)
+		{
+			jumpBufferCounter -= Time.deltaTime;
+		}
 
-			// Jump
-			rb.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+		// Check for jump
+		if (jumpBufferCounter > 0 && IsGrounded())
+		{
+			Jump();
+		}
+
+		// Handle fast falling
+		fastFalling = !Input.GetButton("Jump");
+		if (fastFalling && rb.velocity.y > 0 && !usedFastFall)
+		{
+			usedFastFall = true;
+			rb.AddForce(Vector3.down * rb.velocity.y * letGoOfJumpVelocityDecreaseRatio, ForceMode.VelocityChange);
 		}
 	}
 
@@ -70,5 +90,20 @@ public class PlayerController : MonoBehaviour
 	{
 		// got this from https://discussions.unity.com/t/using-raycast-to-determine-if-player-is-grounded/85134/2
 		return Physics.Raycast(transform.position, Vector3.down, groundedDistFromGround);
+	}
+
+	void Jump()
+	{
+		// Terminate jump buffer counter
+		jumpBufferCounter = 0;
+
+		// Set y velocity to 0 so in case we're falling our jump isn't weakened by the current downwards velocity
+		rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+		// Jump
+		rb.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+
+		// Set usedFastFall
+		usedFastFall = false;
 	}
 }
