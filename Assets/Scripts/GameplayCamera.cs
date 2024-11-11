36a298370d6aa4e96526f122aeffbe393f5fc990
Justin-Lam <justinflam9225k@gmail.com>
2024-11-10 18:51:46 -0800

    Implemented min, max, and smooth zooming

--- Assets/Scripts/GameplayCamera.cs
+++ Assets/Scripts/GameplayCamera.cs
@@ -2,23 +2,27 @@ using UnityEngine;
 
 public class GameplayCamera : MonoBehaviour
 {
-	[Header("Children")]
-	// this double parent setup is necessary so vertical panning doesn't get messed up by vertical rotating
-	// lookAt's forward vector must stay parallel with the ground
-	Transform panner;       // used for panning
-	Transform rotater;      // used for rotating
-	Transform camera;		// used for zooming
-
 	[Header("Panning")]
 	[SerializeField] float panSensitivity;
 
 	[Header("Zooming")]
+	[SerializeField] float initialZoom;		// initial dist cam is from panner
 	[SerializeField] float zoomSensitivity;
-	[SerializeField] float minZoom;
-	[SerializeField] float maxZoom;
+	[SerializeField] float zoomSpeed;
+	[SerializeField] float maxZoomIn;		// min dist cam can be from panner
+	[SerializeField] float maxZoomOut;		// max dist cam can be from panner
+	float targetZoom;						// dist cam should be from panner
+	float currentZoom;						// dist cam is from panner
 
 	[Header("Rotating")]
 	[SerializeField] float rotateSensitivity;
+
+	[Header("Children")]
+	// this double parent setup is necessary so vertical panning doesn't get messed up by vertical rotating
+	// the panner's forward vector must stay parallel with the ground
+	Transform panner;       // for panning
+	Transform rotater;      // for rotating
+	Transform cam;          // for zooming
 
 
 	void Start()
@@ -26,7 +30,11 @@ public class GameplayCamera : MonoBehaviour
 		// Get children
 		panner = transform.GetChild(0);
 		rotater = panner.GetChild(0);
-		camera = rotater.GetChild(0);
+		cam = rotater.GetChild(0);
+
+		// Set current and target zoom levels
+		currentZoom = initialZoom;
+		targetZoom = initialZoom;
 
 		// Set cursor lock state
 		Cursor.lockState = CursorLockMode.Locked;
@@ -48,6 +56,9 @@ public class GameplayCamera : MonoBehaviour
 		}
 
 		// Zooming
-		camera.Translate(0, 0, Input.GetAxis("Mouse Scroll Wheel") * zoomSensitivity, Space.Self);
+		targetZoom -= Input.GetAxis("Mouse Scroll Wheel") * zoomSensitivity;
+		targetZoom = Mathf.Clamp(targetZoom, maxZoomIn, maxZoomOut);
+		currentZoom = Mathf.MoveTowards(currentZoom, targetZoom, zoomSpeed * Time.deltaTime);
+		cam.position = panner.position + cam.forward * -currentZoom;
 	}
 }
