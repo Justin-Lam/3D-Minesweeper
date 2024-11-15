5fcb209b73a451a67b1fc7a6141528b9af5fd294
blytheSchen <blschen@ucsc.edu>
2024-11-14 20:48:22 -0800

    created Block script and GameObjects

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Material dirtColor;
+    public Material mineColor;
+    public enum Type {GRASS, MINE};
+    public Type type;
+
+    private Renderer blockRenderer;
+
+    void Start()
+    {
+        blockRenderer = this.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            OnEat();
+        }
+    }
+
+    void OnEat()
+    {
+        if (type == Type.GRASS)
+        {
+            Debug.Log("You ate grass!");
+            blockRenderer.material = dirtColor;
+        }
+        else if (type == Type.MINE)
+        {
+            Debug.Log("BOOM");
+            blockRenderer.material = mineColor;
+        }
+    }
+}
